943e2751ce4cebf03eaeae3b302a28a3aaf021d7
helenewegener1 <helene.wegener@gmaill.com>
2026-01-19 10:34:26 +0100

    update

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 #PBS -W group_list=dp_immunoth -A dp_immunoth # LARS GROUP
-#PBS -l nodes=1:ppn=64:thinnode
+#PBS -l nodes=1:ppn=32:thinnode
 #PBS -l gpus=0
 #PBS -l mem=128GB
 #PBS -l walltime=70:00:00
 #PBS -e /home/projects/dp_immunoth/people/helweg/projects/benchmarking/running_benchmark.err
 #PBS -o /home/projects/dp_immunoth/people/helweg/projects/benchmarking/running_benchmark.log
 #PBS -N running_benchmark
+
+mkdir -p /home/people/helweg/.conda/pkgs
+mkdir -p /home/people/helweg/.conda/envs
+mkdir -p /home/people/helweg/tmp
+
+export CONDA_PKGS_DIRS=/home/people/helweg/.conda/pkgs
+export CONDA_ENVS_DIRS=/home/people/helweg/.conda/envs
+export TMPDIR=/home/people/helweg/tmp
+export CONDA_TEMP=/home/people/helweg/tmp
+export PIP_CACHE_DIR=/home/people/helweg/.conda/pip
+export CONDA_NO_PLUGINS=true
 
 # SØREN SANDGAARD - WHICH MODULES TO LOAD?
 module load tools 
